d72fbd764b0e0bd85857b22578e1b5c85a72b442
Kris Zyp <kriszyp@gmail.com>
2011-01-04 06:42:20 -0700

    packages.dojofoundation.org is default registry

--- cpm.bat
+++ cpm.bat
@@ -1,3 +1,3 @@
 set CPM_PATH=.
-set CPM_REGISTRY=http://packages.dojotoolkit.org/
+set CPM_REGISTRY=http://packages.dojofoundation.org/
 java -classpath %CPM_PATH%\jars\js.jar org.mozilla.javascript.tools.shell.Main -opt -1 %CPM_PATH%\lib\cpm.js %*
